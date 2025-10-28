eaa37536637a9851373326a49071fe1305fd38b2
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-10-28 18:23:47 -0300

    Feat: Implement Home controller, index and helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resource :session
+  resource :user, only: %i[ new create destroy ]
   resources :passwords, param: :token
+
+  root "home#index"
 end

c4c310b8b11309eb7543f932e002e064aec26f72
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-10-31 13:15:01 -0300

    Refactor: Remove commented JWT token code from create action in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  allow_unauthenticated_access only: %i[ new create ]
+
   def new
     @user = User.new
   end
@@ -7,8 +9,6 @@ class UsersController < ApplicationController
     @user = User.create!(user_params)
 
     if @user.save
-      # token = encode_token(user_id: @user.id)
-      # set_jwt_cookie(token)
       redirect_to new_session_path, notice: "Conta criada com sucesso!"
     else
       redirect_to :new, status: :unprocessable_entity

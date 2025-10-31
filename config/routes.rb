24a29585b04d5b6d603b8586116d0ac1123ae24f
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-10-31 13:14:07 -0300

    Fix: Change user resource to users in routes for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resource :session
-  resource :user, only: %i[ new create destroy ]
+  resources :users, only: %i[ new create destroy ]
   resources :passwords, param: :token
 
   root "home#index"

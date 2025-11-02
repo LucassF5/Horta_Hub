9ddf5a5f433baa54518e6a36c1606dcb5f5fc52b
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-11-01 23:39:40 -0300

    Feat: Create model and migration to Products

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+class Product < ApplicationRecord
+    before_save :format_name
+
+    validates :name, length: { in: 3..50 }, uniqueness: true, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+    private
+
+    def format_name
+        self.name = name.downcase.titleize
+        Rails.logger.info "Formatted product name to: #{name}"
+    end
+end

b8504752621299679a4b363155aea32088dcfdea
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-11-02 00:22:56 -0300

    Feat: Implement Products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,51 @@
+class ProductsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_product, only: [ :show, :edit, :update, :destroy ]
+
+    def index
+        @products = Product.all
+    end
+
+    def show
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+        
+        if @product.save
+            redirect_to root_path, notice: "Produto criado com sucesso"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @product.update!(product_params)
+            redirect_to root_path, notice: "Produto atualizado com sucesso"
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @product.destroy
+        redirect_to root_path, notice: "Produto deletado com sucesso", status: :see_other
+    end
+
+    private
+
+    def product_params
+        params.expect(product: [ :name, :price ])
+    end
+
+    def set_product
+        @product = Product.find(params[:id])
+    end
+end

7e42f7c896c8e8770c87a467d58a8a76f97dd4c6
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-11-21 15:18:38 -0300

    Feat: Add annotate configurations and add model infos

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,3 +12,20 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 30 }
   validates :email_address, presence: true, uniqueness: true
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email_address   :string           not null
+#  password_digest :string           not null
+#  role            :string           default("user"), not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#

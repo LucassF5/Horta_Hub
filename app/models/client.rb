f757a8348f2d955f31e8703f70a53b1af1ab05dc
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-11-21 15:41:36 -0300

    Fix: Rubocop adjust file

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,4 @@
 class Client < ApplicationRecord
-
     validates :name, presence: true, length: { minimum: 3, maximum: 30 }
     validates :phone, presence: true, uniqueness: true
     validates :type, presence: true

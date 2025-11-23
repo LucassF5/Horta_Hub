d9485b9cde0ffc239c0555d042ddaa685ca399e8
Lucas Franco Rocha <franco100lucas@gmail.com>
2025-11-22 23:46:26 -0300

    Refactor: update client model and migration to use client_type instead of type

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,17 +1,22 @@
 class Client < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3, maximum: 30 }
-    validates :phone, presence: true, uniqueness: true
-    validates :type, presence: true
+    validates :phone, uniqueness: true
+    validates :client_type, presence: true
+
+    enum :client_type, {
+        pessoa_fisica: "Pessoa Física",
+        pessoa_juridica: "Pessoa Jurídica"
+    }
 end
 
 # == Schema Information
 #
 # Table name: clients
 #
-#  id         :integer          not null, primary key
-#  name       :string(65)       not null
-#  phone      :string           not null
-#  type       :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  name        :string(65)       not null
+#  phone       :string
+#  client_type :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
--- db/migrate/20251121143806_create_clients.rb
+++ db/migrate/20251121143806_create_clients.rb
@@ -2,8 +2,8 @@ class CreateClients < ActiveRecord::Migration[8.1]
   def change
     create_table :clients do |t|
       t.string :name, limit: 65, null: false
-      t.string :phone, null: false
-      t.string :type, null: false
+      t.string :phone
+      t.string :client_type, null: false
 
       t.timestamps
     end

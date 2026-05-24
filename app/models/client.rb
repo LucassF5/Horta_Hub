b8ff7b4620000e41608e3c677ce2a732b59ed165
Lucas Franco <franco100lucas@gmail.com>
2026-05-24 12:06:04 -0300

    feat: add organizations and memberships tables with foreign key relationships in schema

--- app/models/client.rb
+++ app/models/client.rb
@@ -12,10 +12,15 @@ end
 #
 # Table name: clients
 #
-#  id          :integer          not null, primary key
-#  client_type :string           not null
-#  name        :string(65)       not null
-#  phone       :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id              :integer          not null, primary key
+#  client_type     :string           not null
+#  name            :string(65)       not null
+#  phone           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organization_id :integer          not null
+#
+# Foreign Keys
+#
+#  organization_id  (organization_id => organizations.id)
 #
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  enum :role, {
-    user: "user",
-    admin: "admin"
-  }
-
   validates :username, presence: true, length: { minimum: 3, maximum: 30 }
   validates :email_address, presence: true, uniqueness: true
 end
@@ -20,7 +15,6 @@ end
 #  id              :integer          not null, primary key
 #  email_address   :string           not null
 #  password_digest :string           not null
-#  role            :string           default("user"), not null
 #  username        :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null

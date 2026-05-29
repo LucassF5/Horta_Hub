2802b439cb14b1170a2e91660f5ac4de127bec43
Lucas Franco <franco100lucas@gmail.com>
2026-05-29 05:51:55 -0300

    feat: implement OrganizationsController with new and create actions for organization and user creation

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,35 @@
+class OrganizationsController < ApplicationController
+  unauthenticated_access_only only: %i[new create]
+
+  def new
+    @organization = Organization.new
+    @user = User.new
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+    @user = User.new(user_params)
+
+    ActiveRecord::Base.transaction do
+      @user.save!
+      @organization.save!
+      @organization.memberships.create!(user: @user, role: :owner)
+    end
+
+    start_new_session_for @user
+    redirect_to root_path, notice: "Organização criada com sucesso!"
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:alert] = "Erro ao criar organização. Verifique os dados e tente novamente."
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def organization_params
+    params.expect(organization: [:name])
+  end
+
+  def user_params
+    params.expect(user: [:username, :email_address, :password])
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,7 @@
 class UsersController < ApplicationController
-  unauthenticated_access_only only: %i[ new create ]
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-
-    if @user.save
-      redirect_to new_session_path, notice: "Conta criada com sucesso!"
-    else
-      flash.now[:alert] = "Erro ao criar conta. Verifique os dados e tente novamente."
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def destroy
     terminate_session
     Current.user.destroy
     redirect_to new_session_path, notice: "Conta excluída com sucesso."
-  end
-
-  private
-
-  def user_params
-    params.expect(user: [ :email_address, :password, :username ])
   end
 end

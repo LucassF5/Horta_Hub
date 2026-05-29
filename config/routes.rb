4ded24117e1689328caad353365e2a89ffb4d45e
Lucas Franco <franco100lucas@gmail.com>
2026-05-29 05:51:55 -0300

    feat: implement OrganizationsController with new and create actions for organization and user creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resource :session
-  resources :users, only: %i[ new create destroy ]
+  resources :users, only: %i[ destroy ]
   resources :passwords, param: :token
+  resources :organizations, only: %i[ new create ]
   resources :products
   resources :clients
 

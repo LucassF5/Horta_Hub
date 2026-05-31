5f50f16b73d5f8086a5307b7f2590d815b646a44
Lucas Franco <franco100lucas@gmail.com>
2026-05-31 19:25:25 -0300

    feat: refactor ClientsController to use Phlex views and add request specs for client management

---
+++ spec/models/client_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Client, type: :model do
+  describe 'associations' do
+    it { should belong_to(:organization) }
+  end
+
+  describe 'validations' do
+    subject { build(:client) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:client_type) }
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(30) }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:client_type).backed_by_column_of_type(:string).with_values(pessoa_fisica: "Pessoa Física", pessoa_juridica: "Pessoa Jurídica") }
+  end
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      expect(build(:client)).to be_valid
+    end
+
+    it 'creates pessoa_juridica clients' do
+      client = build(:client, :pessoa_juridica)
+      expect(client.client_type).to eq("pessoa_juridica")
+    end
+
+    it 'creates clients without phone' do
+      client = build(:client, :without_phone)
+      expect(client.phone).to be_nil
+      expect(client).to be_valid
+    end
+  end
+
+  describe 'name validation' do
+    it 'is invalid with name too short' do
+      client = build(:client, name: "ab")
+      expect(client).not_to be_valid
+    end
+
+    it 'is invalid with name too long' do
+      client = build(:client, name: "a" * 31)
+      expect(client).not_to be_valid
+    end
+  end
+end

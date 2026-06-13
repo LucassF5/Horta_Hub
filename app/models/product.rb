9486877387fda9a3dcff343676c311accf20b7cb
Lucas Franco <franco100lucas@gmail.com>
2026-06-13 06:21:21 -0300

    feat: implement friendly_id for clients, organizations, and products to manage non-evident private IDs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,20 @@
 class Product < ApplicationRecord
+    extend FriendlyId
+
     belongs_to :organization
     has_many :sale_items, dependent: :restrict_with_error
 
     before_save :format_name
 
+    friendly_id :name, use: [ :slugged, :scoped, :history ], scope: :organization
+
     validates :name, length: { in: 3..50 }, uniqueness: { scope: :organization_id }, presence: true
     validates :price, numericality: { greater_than: 0 }, presence: true
+    validates :slug, presence: true, uniqueness: { scope: :organization_id }
+
+    def should_generate_new_friendly_id?
+        slug.blank? || (will_save_change_to_name? && !will_save_change_to_slug?)
+    end
 
     private
 
@@ -22,9 +31,14 @@ end
 #  id              :integer          not null, primary key
 #  name            :string           not null
 #  price           :decimal(10, 2)   not null
+#  slug            :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  organization_id :integer          not null
+#
+# Indexes
+#
+#  index_products_on_organization_id_and_slug  (organization_id,slug) UNIQUE
 #
 # Foreign Keys
 #
